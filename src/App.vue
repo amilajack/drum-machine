c626c15ef710828f12b59f8dd1be386a745a1caa
Ohad Ron <inverted3@gmail.com>
2017-09-16 11:29:13 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <machine></machine>
+  </div>
+</template>
+
+<script>
+import Machine from './components/Machine'
+
+export default {
+  name: 'app',
+  components: {
+    Machine
+  }
+}
+</script>
+
+<style>
+#app{
+  margin: 0 auto;
+  width: 900px;
+}
+body {
+  font-family: BlinkMacSystemFont,  sans-serif;
+  user-select: none;
+  background-color: #eee;
+}
+</style>
