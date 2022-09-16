6aad12066c7e9b991b280bacd69d9fcb42410285
Amila Welihinda <amilajack@gmail.com>
2022-09-16 16:21:52 -0700

    feat: integrate palette

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,72 @@
 </template>
 
 <script>
-import Machine from './components/Machine.vue';
+import "virtual:@palette.dev/plugin-vite/init";
+import {
+  init,
+  events,
+  vitals,
+  measure,
+  network,
+  profiler,
+  label,
+} from "@palette.dev/browser";
+
+init({
+  key: "cl8507e78001209miieofddqt",
+  // Collect click, web vitals, network, performance events, and profiles
+  plugins: [events(), vitals(), network(), measure(), profiler()],
+});
+
+// Profile page load
+profiler.start({ sampleInterval: 10, maxBufferSize: 10_000 });
+addEventListener("load", () => profiler.stop());
+
+// A utility for profiling and label frequent events
+const debounce = (start, stop, opts = { timeout: 1_000 }) => {
+  let timeoutId;
+  return () => {
+    if (timeoutId == undefined) {
+      start();
+    } else {
+      clearTimeout(timeoutId);
+    }
+    // Debounce marking the end of the label
+    timeoutId = setTimeout(() => {
+      stop();
+      timeoutId = undefined;
+    }, timeoutId);
+  };
+};
+
+// Debounce starting the profiler
+const debounceProfiler = debounce(
+  () => {
+    label.start("ui.interaction");
+    profiler.start({
+      sampleInterval: 10,
+      maxBufferSize: 10_000,
+    });
+  },
+  () => {
+    label.end("ui.interaction");
+    return profiler.stop();
+  }
+);
+
+// Profile scroll, mousemove, and click events
+addEventListener("wheel", debounceProfiler);
+addEventListener("mousemove", debounceProfiler);
+addEventListener("click", debounceProfiler);
+addEventListener("keypress", debounceProfiler);
+
+import Machine from "./components/Machine.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Machine
-  }
+    Machine,
+  },
 };
 </script>
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import palette from "@palette.dev/plugin-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    palette({
+      key: process.env.PALETTE_ASSET_KEY,
+      outputPath: "dist/assets",
+    }),
+  ],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        sourcemapPathTransform: (relativeSourcePath, sourcemapPath) =>
+          path.relative(
+            process.cwd(),
+            path.resolve(path.dirname(sourcemapPath), relativeSourcePath)
+          ),
+      },
+    },
+  },
 });
